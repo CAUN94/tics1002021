65b9b7b79eae7463d1b67182fcc317d5d01dbf76
caun94 <cristobalugarte6@gmail.com>
2021-03-16 10:51:30 -0300

    Clase 1

---
+++ clase1/clase1.py
@@ -0,0 +1,10 @@
+# Mostrando Datos
+print('Hello World')
+
+# Interactuando con el usario
+numero = input('Dame un nr: ')
+print(f"Tu numero es: {numero}")
+
+# Haciendo una tarea muchas veces
+for i in range(20):
+	print(i)
